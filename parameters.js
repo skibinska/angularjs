3d2fc48168f9ba181c6d49e800cb558623f13700
skibinska <skibinska.ewelina@gmail.com>
2015-10-09 20:45:43 +0100

    iniitial commit

---
+++ parameters.js
@@ -0,0 +1,10 @@
+(function () {
+    'use restrict';
+    angular
+        .module("sportStore")
+        .constant("DeploydParameters", {
+            gateway_endpoint: 'http://localhost:2403'
+
+        })
+})();
+    
